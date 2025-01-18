b3835984707832939d7f37fcec8974cc61e75d78
fpauly <fpauly@hotmail.com>
2025-01-18 21:49:17 +0200

    update

--- part1/CourseInfo/src/App.jsx
+++ part1/CourseInfo/src/App.jsx
@@ -1,34 +1,3 @@
-const App = () => {
- 
-
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-  return (
-    <div>
-     
-      <Header course = {course}/>
-      <Content parts = {course.parts}/>
-      <Total parts = {course.parts}/>
-    </div>
-  )
-}
-
 const Header = (props) => {
   return (
     <div>
@@ -60,5 +29,38 @@ const Total = (props) => {
     </div>
   )
 }
+
+const App = () => {
+ 
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+     
+      <Header course = {course}/>
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
+    </div>
+  )
+}
+
+
 
 export default App
