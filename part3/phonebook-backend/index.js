d418d1c4a99abc8e8db84641ad114c3b6b30c140
Fan Yin <fpauly@hotmail.com>
2026-01-03 23:46:47 +0200

    part 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -32,6 +32,17 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find(p=>p.id===id);
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+});
+
+
 app.get("/info", (request, response) => {
   date = new Date();
   response.send(`<p>Phonebook has info for ${persons.length} people</p>
