a084f1895e34868c1bbfe3253c81e32288a973cd
Fan Yin <fpauly@hotmail.com>
2026-02-20 19:32:29 +0200

    Exercise 4.4

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,13 +1,13 @@
- 
-const dummy = (blogs) =>{
-  return blogs? 1:1
+const dummy = (blogs) => {
+  return blogs ? 1 : 1
 }
 
-const totalLikes = (blogs) =>{
-  return blogs.reduce((sum , b)=> sum+b.likes,0)
+const totalLikes = (blogs) => {
+  // console.log(blogs.reduce((sum, b) => sum + b.likes, 0))
+  return blogs.reduce((sum, b) => sum + b.likes, 0)
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
 }
