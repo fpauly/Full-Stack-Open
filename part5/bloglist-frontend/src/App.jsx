a03e8b2e7401e9a2624c2f573b67821e0f89e0c7
fpauly@hotmail.com <fpauly@hotmail.com>
2026-06-26 15:35:14 +0300

    WIP

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,8 +2,11 @@ import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState['']
+  const [password, setPassword] = useState['']
 
   const fetchBlogs = async () => {
     const blogs = await blogService.getAll()
@@ -13,8 +16,37 @@ const App = () => {
     fetchBlogs()
   }, [])
 
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('loing in with', username, password)
+  }
+
   return (
     <div>
+      <h2>login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor='username' >username</label>
+          <input type='text'
+            id='username'
+            value={username}
+            onChange={({ target }) => setUsername(target.value)} />
+        </div>
+
+
+        <div>
+          <label>
+            password
+            <input type='password'
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type='submit'>login</button>
+      </form>
+      <br></br>
+      <p>below is for testing</p>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
