be542f5a259dfa4e9bf68aeb2baa5a5644c306d0
fpauly@hotmail.com <fpauly@hotmail.com>
2026-07-01 09:34:00 +0300

    part5 init

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
-
+import Notification from './components/Notification'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState['']
-  const [password, setPassword] = useState['']
+  const [message, setMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
 
   const fetchBlogs = async () => {
     const blogs = await blogService.getAll()
@@ -16,35 +20,56 @@ const App = () => {
     fetchBlogs()
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
+
+    try {
+      const userData = await loginService.login({ username, password })
+      setUser(userData)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setMessage('Wrong credentials')
+      setTimeout(() => {
+        setMessage(null)
+      }, 3000)
+    }
     console.log('loing in with', username, password)
   }
 
   return (
     <div>
       <h2>login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label htmlFor='username' >username</label>
-          <input type='text'
-            id='username'
-            value={username}
-            onChange={({ target }) => setUsername(target.value)} />
-        </div>
+      <Notification message={message} />
 
 
-        <div>
-          <label>
-            password
-            <input type='password'
-              value={password}
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </label>
-        </div>
-        <button type='submit'>login</button>
-      </form>
+      {user ?
+        (<div>{user.username}</div>)
+
+        : (
+          <form onSubmit={handleLogin}>
+            <div>
+              <label htmlFor='username' >username</label>
+              <input type='text'
+                id='username'
+                value={username}
+                onChange={({ target }) => setUsername(target.value)} />
+            </div>
+
+
+            <div>
+              <label>
+                password
+                <input type='password'
+                  value={password}
+                  onChange={({ target }) => setPassword(target.value)}
+                />
+              </label>
+            </div>
+            <button type='submit'>login</button>
+          </form>
+        )}
+
       <br></br>
       <p>below is for testing</p>
       <h2>blogs</h2>
