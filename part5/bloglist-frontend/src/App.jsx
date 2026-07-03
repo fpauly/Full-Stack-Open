921d81e221714c27d2ecf85f6507ba8783e6ec80
fpauly@hotmail.com <fpauly@hotmail.com>
2026-07-03 10:56:40 +0300

    WIP

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -42,7 +42,7 @@ const App = () => {
   }
 
 
-  if (user === null) {
+  const loginForm = () => {
     return (
       <div>
         <h2>Log in to application</h2>
@@ -72,13 +72,25 @@ const App = () => {
 
     )
   }
+
+  const blogForm = () => {
+    return (
+      <div>
+        <h2>blogs</h2>
+        <label>{user.name} logged in</label>
+
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {!user && loginForm()}
+      {user && blogForm()}
     </div>
+
   )
 }
 
