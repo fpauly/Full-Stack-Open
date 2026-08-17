6fc6dc5297b4ad6ac7feb1b1a8d4ba877aec9ad4
fpauly@hotmail.com <fpauly@hotmail.com>
2026-08-17 23:15:14 +0300

    WIP

---
+++ part5/bloglist-frontend/src/test/Blog.test.jsx
@@ -0,0 +1,5 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import BlogItem from '../components/BlogItem'
+
+test('test', () => { })
